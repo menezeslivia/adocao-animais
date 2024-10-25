42b6d44b73a33c313d745840711c1c089c11eafd
Maria Fernanda <124320003+nanbuonafina@users.noreply.github.com>
2024-10-25 10:32:42 -0300

    :sparkles: feat: Cabeçalho da HomePage

---
+++ sistema_adocao/src/app/componentes/cabecalho/cabecalho.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-cabecalho',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './cabecalho.component.html',
+  styleUrl: './cabecalho.component.css'
+})
+export class CabecalhoComponent {
+
+}

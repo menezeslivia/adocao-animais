f4709430aad67aca9c0ef8428e30c7706a65ec6a
Maria Fernanda <124320003+nanbuonafina@users.noreply.github.com>
2024-10-25 19:51:44 -0300

    Add o background

--- sistema_adocao/src/app/app.component.ts
+++ sistema_adocao/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {CabecalhoComponent} from './componentes/cabecalho/cabecalho.component';
 import {MainComponent} from './componentes/main/main.component';
 import {ButtonComponent} from './componentes/button/button.component';
+import {BackgroundComponent} from './componentes/background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent, MainComponent, ButtonComponent],
+  imports: [RouterOutlet, CabecalhoComponent, MainComponent, ButtonComponent, BackgroundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

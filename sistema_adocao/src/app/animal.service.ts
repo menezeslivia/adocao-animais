5b4258cbd310a730eeb2cb92364ba94d6758cb3b
Maria Fernanda <124320003+nanbuonafina@users.noreply.github.com>
2024-11-03 18:15:14 -0300

    :wastebasket: remove: Removendo arquivos que nao vamos usar

--- sistema_adocao/src/app/animal.service.ts
+++ sistema_adocao/src/app/animal.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
 
+interface Animal {
+  id: number;
+  name: string;
+  type: string;
+  age: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalService {
+  private animals: Animal[] = [
+    { id: 1, name: 'Bobby', type: 'Dog', age: 3 },
+    { id: 2, name: 'Milo', type: 'Cat', age: 2 }
+  ];
+  private nextId = 3;
 
-  constructor() { }
+  getAnimals() {
+    return this.animals;
+  }
+
+  addAnimal(animal: Animal) {
+    animal.id = this.nextId++;
+    this.animals.push(animal);
+  }
+
+  updateAnimal(updatedAnimal: Animal) {
+    const index = this.animals.findIndex(a => a.id === updatedAnimal.id);
+    if (index !== -1) this.animals[index] = updatedAnimal;
+  }
+
+  deleteAnimal(id: number) {
+    this.animals = this.animals.filter(animal => animal.id !== id);
+  }
 }

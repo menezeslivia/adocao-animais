0bfcb1fdbf03a022394c06586cc27e0e74da1e39
Maria Fernanda <124320003+nanbuonafina@users.noreply.github.com>
2024-11-03 19:49:31 -0300

    erro erro

--- sistema_adocao/src/app/app.component.ts
+++ sistema_adocao/src/app/app.component.ts
@@ -3,16 +3,13 @@ import { Router, RouterOutlet } from '@angular/router';
 import {CabecalhoComponent} from './componentes/cabecalho/cabecalho.component';
 import {MainComponent} from './componentes/main/main.component';
 import {ButtonComponent} from './componentes/button/button.component';
-// import {BackgroundComponent} from './componentes/background/background.component';
 import {SobreComponent} from './componentes/sobre/sobre.component';
-import {AnimaisComponent} from './componentes/animais/animais.component';
-import {CardAnimalComponent} from './componentes/animais/card-animal/card-animal.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent, MainComponent, ButtonComponent, SobreComponent, AnimaisComponent, CardAnimalComponent, CommonModule],
+  imports: [RouterOutlet, CabecalhoComponent, MainComponent, ButtonComponent, SobreComponent, CommonModule],
   host: { ngSkipHydration: 'true' },
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
